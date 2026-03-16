844ae4face2d518f7a625446835aadcefd017daf
X S Minisha Alias Lincy <minishalincy123@gmail.com>
2026-03-16 10:28:47 +0530

    array methods

---
+++ Arrays/arrayMethods.js
@@ -0,0 +1,31 @@
+
+//homogenous array
+let users = ["mini","lincy"]
+console.log(users);
+
+//heterogenous array
+let user1 = ['linc',null,undefined,10]
+console.log(user1);
+
+
+//array methods
+let user2 = ['mini','lincy']
+console.log(user2);
+
+//push()
+user2.push('MinishaLincy')
+console.log(user2);
+
+//pop()
+user2.pop()
+console.log(user2);
+
+//unshift
+user2.unshift('minisha')
+console.log(user2);
+
+//shift
+user2.shift()
+console.log(user2);
+
+

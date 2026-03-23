85cea16aa13ffc211ec4cdb25ce937b7b695c03f
X S Minisha Alias Lincy <minishalincy123@gmail.com>
2026-03-23 10:40:48 +0530

    objects

---
+++ Objects/object.js
@@ -0,0 +1,19 @@
+let employee={
+    empID:181,
+    name:'minisha',
+    designation:'SDE',
+    work: function(){
+        console.log('Building frontend and backend application');
+    },
+
+    location:{
+        state:"Telangana",
+        city:'Hyderabad',
+    },
+
+    technologies:['Javascript','Nodejs','Expressjs','MongoDB'],
+
+
+}
+
+console.log(employee);
